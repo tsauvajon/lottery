7c3af898d9ca4c0339e65bf4c53d4840139ba13d
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2018-04-19 21:55:20 +0200

    Add the Lottery contract

---
+++ src/contracts/Lottery.sol
@@ -0,0 +1,46 @@
+pragma solidity ^0.4.21;
+
+import "./Killable.sol";
+
+contract Lottery is Killable {
+    mapping(address => uint) usersBet;
+    mapping(uint => address) users;
+
+    uint nbUsers = 0;
+    uint totalBets = 0;
+
+    modifier hasValue() {
+        require(msg.value > 0);
+        _;
+    }
+
+    modifier onlyIf(bool _condition) {
+        require(_condition);
+        _;
+    }
+
+    function bet() public payable hasValue {    
+        if (usersBet[msg.sender] == 0) { // new player
+            users[nbUsers] = msg.sender;
+            nbUsers++;
+        }
+
+        usersBet[msg.sender] += msg.value;
+        totalBets += msg.value;
+    }
+
+    function endLottery() public onlyOwner returns (address) {
+        uint sum = 0;
+        uint winningNumber = uint(block.blockhash(block.number - 1)) % totalBets;
+
+        for (uint i = 0; i < nbUsers; i++) {
+            sum += usersBet[users[i]];
+
+            if (sum >= winningNumber) {
+                // destroy this contract and send the balance to users[i]
+                selfdestruct(users[i]);
+                return users[i];
+            }
+        }
+    }
+}
