d62c5b8bf96fba3682221e31198a80d062a6715c
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2018-04-20 19:00:46 +0200

    Get assets to work

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './lottery',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
